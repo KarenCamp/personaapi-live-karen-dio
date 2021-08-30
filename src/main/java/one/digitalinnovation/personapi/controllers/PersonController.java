cfa899cf85b8d25d93065d62e4904a94187424ab
KarenCamp <karen-campinas@hotmail.com>
2021-08-30 17:28:21 -0300

    Created README.md file

--- src/main/java/one/digitalinnovation/personapi/controllers/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controllers/PersonController.java
@@ -27,19 +27,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 
 public class PersonController {
 
     private PersonService personService;
 
-   @Autowired
-  public PersonController(PersonService personService) {
-          this.personService = personService;
- }
-
-   @PostMapping
-   @ResponseStatus(HttpStatus.CREATED)
-   public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
     return personService.createPerson(personDTO);
   }
 
